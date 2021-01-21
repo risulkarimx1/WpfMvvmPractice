5d5597a82a9e011a2e27405cf3d4f4499e9307f6
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-21 02:47:15 +0100

    added simple application

---
+++ WpfDempTimCorey.App/WpfDempTimCorey/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace WpfDempTimCorey
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Person> _people;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _people = new List<Person>()
+            {
+                new Person {FirstName = "Risul", LastName = "Karim"},
+                new Person {FirstName = "Rafia", LastName = "Anjum"}
+            };
+
+            MyComboBox.ItemsSource = _people;
+        }
+
+        private void SubmitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {FirstNameText.Text}");
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
